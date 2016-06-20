470e357b76d8374a9f8cb23fdd4d7c7e014d1b42
maxim <o.yakubenko2014@yandex.ua>
2016-06-20 21:34:17 +0300

    third commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // model.php
 require_once 'model.php';
 
-$_posts = get_all_posts
+$posts = get_all_posts();
 
 require 'templates/list.php';
 ?>
---
+++ templates/list.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxim
+ * Date: 20.06.16
+ * Time: 21:32
+ */
