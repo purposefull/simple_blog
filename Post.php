3dcb7148da2cbe3aa5836cd27e0daa6afbd56034
maxim <o.yakubenko2014@yandex.ua>
2016-07-28 17:50:07 +0300

    class

--- Post.php
+++ Post.php
@@ -2,8 +2,47 @@
 class Post
 {
 public $title = 'title';
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
 public $body = 'body';
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
 public $created_at = 'date';
+
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
 }
 
+
+
+
+
 $post1 = new Post;
+
+echo '<pr>';
+
+print_r($post1);
