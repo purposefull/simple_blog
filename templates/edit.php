69a9fdd016ac23a5d81a1886241b4c9247038115
maxim <o.yakubenko2014@yandex.ua>
2016-08-03 16:52:50 +0300

    fixed tmeplate/edit.php

--- templates/edit.php
+++ templates/edit.php
@@ -1,9 +1,9 @@
 <h1>Edit Post</h1>
 
 <form action="" method="post">
-    <input type="hidden"  name="id" value="<?php echo $data['id'] ?>"><br>
-    <input type="text" placeholder="title" name="title" value="<?php echo $data['title'] ?>"><br>
-    <input type="text" placeholder="body" name="body" value="<?php echo $data['body'] ?>"><br>
-    <input type="text" placeholder="created_at" name="created_at" value="<?php echo $data['created_at'] ?>"><br><br>
+    <input type="hidden"  name="id" value="<?php echo $BlogPost->getId();  ?>"><br>
+    <input type="text" placeholder="title" name="title" value="<?php echo $BlogPost->getTitle(); ?>"><br>
+    <input type="text" placeholder="body" name="body" value="<?php echo $BlogPost->getBody(); ?>"><br>
+    <input type="text" placeholder="created_at" name="created_at" value="<?php echo $BlogPost->getCreatedAt(); ?>"><br><br>
     <input type="submit" name="send">
 </form>
